18b520d42345037d430122a5f2a9c48d1c30696e
Lucianocbarbosa <lucianobebarbosa@gmail.com>
2022-01-19 10:00:31 -0300

    Serviços de menuNavegador e Dashboard em Json

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,6 +1,11 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+import { Dashboard } from '../modelosInterface/dashboard';
+import { DashboardService } from './../servicosInterface/dashboard.service';
+
 
 @Component({
   selector: 'app-feed',
@@ -8,26 +13,31 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent {
+  cards$: Observable<Dashboard[]>;
   usuario = { userName: 'Luciano Barbosa', icone: 'remember_me' };
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Melhor livro de Janeiro', img: '../../assets/imagens/1.png', cols: 1, rows: 1 },
-          { title: 'Dica dos leitores', img: '../../assets/imagens/2.png', cols: 1, rows: 1 },
-          { title: 'O mais comentados da semana', img: '../../assets/imagens/3.png', cols: 1, rows: 1 },
-          { title: 'Indicação do Tiime BookShelf', img: '../../assets/imagens/4.png', cols: 1, rows: 1 }
+
         ];
       }
 
       return [
-        { title: 'Melhor livro de Janeiro', img: '../../assets/imagens/1.png', cols: 2, rows: 1 },
-        { title: 'Dica dos leitores', img: '../../assets/imagens/2.png', cols: 1, rows: 1 },
-        { title: 'O mais comentados da semana', img: '../../assets/imagens/3.png', cols: 1, rows: 2 },
-        { title: 'Indicação do Tiime BookShelf', img: '../../assets/imagens/4.png', cols: 1, rows: 1 }
+        this.cards$
       ];
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private dashboardService: DashboardService
+  ) {
+    this.cards$ = dashboardService.listagemCards()
+      .pipe(
+        catchError(error => {
+          return of([])
+        })
+      )
+  }
 }
--- src/app/navegacao/navegacao.component.ts
+++ src/app/navegacao/navegacao.component.ts
@@ -1,7 +1,10 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+
+import { MenuNavegador } from '../modelosInterface/menu-navegador';
+import { NavegacaoService } from './../servicosInterface/navegacao.service';
 
 @Component({
   selector: 'app-navegacao',
@@ -9,30 +12,31 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./navegacao.component.scss']
 })
 export class NavegacaoComponent {
-
-  // Itens do menu principal
+  //Itens co menu principal.
   tituloNav = 'BookShelf v1';
-  usuario = { userName: "Luciano Barbosa", icone: "remember_me" }
+  usuario = { userName: 'Victor Icoma', icone: 'remember_me' };
+  //Itens da Barra superior.
   tituloBarra = '[Sua Estante Virtual]';
+  //Itens de icones e imagens de navegação.
   iconeGeral = '../../assets/imagens/ShelfBook.png';
   lIcone = 80;
   aIcone = 80;
-  // Controles das rotas do menu.
-  itensMenu = [
-    { linkMenu: '/cdd', labelMenu: 'Classes Dewey', hab: true },
-    { linkMenu: '/feed', labelMenu: 'Feed Notícias', hab: true },
-    { linkMenu: '/clube', labelMenu: 'Página Usuário', hab: false },
-    { linkMenu: '/leitura', labelMenu: 'Clubes e Leitura', hab: false },
-    { linkMenu: '/estante', labelMenu: 'Estante Particular', hab: false }
-  ]
-
-
+  //Controle das rotas do menu.
+  itensMenu$: Observable<MenuNavegador[]>;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
-
-  constructor(private breakpointObserver: BreakpointObserver) { }
-
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private navegadorService: NavegacaoService
+  ) {
+    this.itensMenu$ = navegadorService.listagemMenu()
+      .pipe(
+        catchError(error => {
+          return of([])
+        })
+      )
+  }
 }
